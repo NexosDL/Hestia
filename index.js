814e4a1edc25563c7aa24a2957282a83e3825bcf
NexosDL <43483660+NexosDL@users.noreply.github.com>
2018-09-22 23:34:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-bot.login("NDkzMTY2OTI5MjY4MzEwMDE3.DohBvg.MEpOzgpEL7bvXtCTjGDj8OD9uZU")
+bot.login(process.env.TOKEN)
 var prefix = ("?")
 
 bot.on('ready', function() {
