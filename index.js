05870b7e6c30dbbfb90aaa87d7dacb2f548fa0fb
NexosDL <43483660+NexosDL@users.noreply.github.com>
2018-09-29 13:21:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,31 +2,3 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 bot.login(process.env.TOKEN)
 var prefix = ("?")
-
-bot.on('ready', function() {
-    bot.user.setPresence({ game: { name: 'Nexøs. ', type: 2} })
-    bot.user.setStatus('dnd');
-    console.log("Connected");
-});
-bot.on('message', message => {
-    if (message.content === prefix + ""){
-        var imagetest = new Discord.RichEmbed()
-            message.channel.sendEmbed(imagetest);
-            message.delete()
-    }
-})
-bot.on('guildMemberAdd', member => {
-    var role = member.guild.roles.find('name', '👤 | Nouveau')
-    member.addRole(role)
-})
-bot.on("guildMemberAdd", member => {
-    const bvn = member.guild.channels.find(m => m.name === "welcome")
-    if (!bvn) return;
-    const embed = new Discord.RichEmbed()
-    .setColor('#009114')
-    .setAuthor(member.user.tag)
-    .setThumbnail(member.user.avatarURL)
-    .addField(`Nombre de membres après son arrivée`, member.guild.memberCount)
-    .setTimestamp()
-    bvn.send(embed)
-})
