10eed1b1d64d155df7e9f1d3603918db26bf9929
NexosDL <43483660+NexosDL@users.noreply.github.com>
2018-10-10 20:45:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,31 +2,16 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 bot.login(process.env.TOKEN)
 var prefix = ("?")
+const ytdl = require('ytdl-core');
+const queue = new Map();
+var servers = {};
 
 bot.on('ready', function() {
-    bot.user.setPresence({ game: { name: 'VSCODE ', type: 3} })
+    bot.user.setPresence({ game: { name: 'la modération', type: 2} })
     bot.user.setStatus('dnd');
-    console.log("Connected");
-});
-bot.on('message', message => {
-    if (message.content === prefix + ""){
-        var imagetest = new Discord.RichEmbed()
-            message.channel.sendEmbed(imagetest);
-            message.delete()
-    }
+    console.log(`Logged in as ${bot.user.tag}!`);
 })
 bot.on('guildMemberAdd', member => {
-    var role = member.guild.roles.find('name', '👤 | Nouveau')
+    var role = member.guild.roles.find('name', '👤 | Abonné')
     member.addRole(role)
-})
-bot.on("guildMemberAdd", member => {
-    const bvn = member.guild.channels.find(m => m.name === "welcome")
-    if (!bvn) return;
-    const embed = new Discord.RichEmbed()
-    .setColor('#009114')
-    .setAuthor(member.user.tag)
-    .setThumbnail(member.user.avatarURL)
-    .addField(`Nombre de membres après son arrivée`, member.guild.memberCount)
-    .setTimestamp()
-    bvn.send(embed)
 })
